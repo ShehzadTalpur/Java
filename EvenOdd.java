75fdb270270854bd9e9c546957623a3c0a6ef26b
ShehzadTalpur <shehzadtalpur12@gmail.com>
2023-12-31 14:17:36 +0500

    WAP to check for even or odd

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class EvenOdd {
+public static void main(String[] args) {
+int no=90;
+if (no%2==0) 
+{
+System.out.println("Number is even");    
+} 
+else{
+    System.out.println("Number is odd");
+}
+}    
+}
