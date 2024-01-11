8b79393c61d5a4bb30392c91b8ec996e6599012f
ShehzadTalpur <shehzadtalpur12@gmail.com>
2024-01-11 10:53:31 +0500

    Print multiplication table

---
+++ MultiT.java
@@ -0,0 +1,18 @@
+package Java;
+
+import java.util.Scanner;
+
+public class MultiT {
+    public static void main(String[] args) {
+        //Print multiplication table
+        int t;
+        Scanner Ob=new Scanner(System.in);
+        System.out.println("Please enter number");
+        t=Ob.nextInt();
+        System.out.println("\nTable of "+t+"\n");
+        for(int i =1; i<=12;i++){
+            System.out.println(t+" x "+i+" = "+t*i);
+        }
+    }
+    
+}
